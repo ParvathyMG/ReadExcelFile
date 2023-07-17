576123e290a1c9e6b406ef5fbf92e73f0deec0e9
ParvathyMG <parvathymohan24.7@gmail.com>
2023-07-17 21:48:22 +0530

    Second commit

--- src/test/java/utils/ExcelUtils.java
+++ src/test/java/utils/ExcelUtils.java
@@ -1,45 +1,33 @@
 package utils;
+import java.io.File;  
+import java.io.FileInputStream;  
+import java.io.IOException;  
+import org.apache.poi.xssf.usermodel.XSSFSheet;  
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;  
+import org.apache.poi.ss.usermodel.Cell;  
+import org.apache.poi.ss.usermodel.FormulaEvaluator;  
+import org.apache.poi.ss.usermodel.Row;  
+public class ExcelUtils  
+{  
+public static void main(String args[]) throws IOException  
+{  
+  
+FileInputStream fis=new FileInputStream(new File("C:\\Users\\Krishnaprasad\\eclipse-workspace\\ExcelUtility\\data\\TestData.xlsx.XLSX"));  
+  
+XSSFWorkbook wb=new XSSFWorkbook(fis);   
+  
+XSSFSheet sheet=wb.getSheetAt(0);  
+   
+FormulaEvaluator formulaEvaluator=wb.getCreationHelper().createFormulaEvaluator();  
 
-import java.io.IOException;
+for(Row row: sheet)      
+{  
+for(Cell cell: row)     
+{  
+System.out.println(cell);
+}  
+}  
+System.out.println();  
+}  
+}  
 
-import org.apache.poi.ss.usermodel.DataFormatter;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
-
-public class ExcelUtils 
-{
-	public static void main(String[] args) throws Exception
-	{
-		getRowCount();
-		getCellData();
-	}
-	public static void getCellData() throws IOException
-	{
-		
-		String excelPath=".\\data\\TestData.xlsx";
-		XSSFWorkbook workbook=new XSSFWorkbook(excelPath);
-		XSSFSheet sheet=workbook.getSheet("Sheet1");
-		
-		
-		String value=sheet.getRow(1).getCell(2).getStringCellValue();
-		System.out.println(value);
-	}
-	public static void getRowCount()
-	{
-		try
-		{
-		String excelPath="./data/TestData.xlsx";
-		XSSFWorkbook workbook=new XSSFWorkbook(excelPath);
-		XSSFSheet sheet=workbook.getSheet("Sheet1");
-		int rowCount=sheet.getPhysicalNumberOfRows();
-		System.out.println("No of rows : "+rowCount);
-		}catch (Exception exp)
-		{
-			System.out.println(exp.getCause());
-			System.out.println(exp.getMessage());
-			exp.printStackTrace();
-		}
-	}
-
-}
